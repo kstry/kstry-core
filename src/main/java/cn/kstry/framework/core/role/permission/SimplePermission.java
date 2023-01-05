65a2bf7ae336feee534628ca07cd7e086a813ea1
lykan <jiashuomeng@gmail.com>
2023-01-06 03:25:19 +0800

    开源协议修改

--- src/main/java/cn/kstry/framework/core/role/permission/SimplePermission.java
+++ src/main/java/cn/kstry/framework/core/role/permission/SimplePermission.java
@@ -1,6 +1,6 @@
 /*
  *
- *  * Copyright (c) 2020-2022, Lykan (jiashuomeng@gmail.com).
+ *  * Copyright (c) 2020-2023, Lykan (jiashuomeng@gmail.com).
  *  * <p>
  *  * Licensed under the Apache License, Version 2.0 (the "License");
  *  * you may not use this file except in compliance with the License.
