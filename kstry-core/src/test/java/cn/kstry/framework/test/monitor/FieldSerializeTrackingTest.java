5967c56791d0521f52b07aa61dfce000a4676f33
xiongzhongwei <xiongzhongwei@bytedance.com>
2023-12-23 16:12:56 +0800

    【add test for monitor】

---
+++ kstry-core/src/test/java/cn/kstry/framework/test/monitor/FieldSerializeTrackingTest.java
@@ -0,0 +1,57 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2023 bytedance.com. All Rights Reserved
+ * This file is part of bytedance EA project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package cn.kstry.framework.test.monitor;
+
+import cn.kstry.framework.core.constant.GlobalProperties;
+import cn.kstry.framework.core.enums.ScopeTypeEnum;
+import cn.kstry.framework.core.monitor.FieldSerializeTracking;
+import cn.kstry.framework.core.monitor.ParamTracking;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * test for Field Serialize Tracking
+ *
+ * @author xiongzhongwei<xiongzhongwei @ bytedance.com>
+ * @date 12/21/2023 9:18 PM
+ */
+public class FieldSerializeTrackingTest {
+
+    @Test
+    public void testValueOfNull() {
+        // test：when passValue is null
+        ParamTracking paramTracking1 = ParamTracking.build(
+                "param", "source", ScopeTypeEnum.REQUEST,
+                null, String.class, "converter"
+        );
+
+        FieldSerializeTracking fieldSerializeTracking1 = new FieldSerializeTracking();
+        String serializedValue1 = fieldSerializeTracking1.valueSerialize(paramTracking1);
+        Assert.assertNull(serializedValue1);
+    }
+
+    @Test
+    public void testMaxLengthError() {
+        // test: Can the length be truncated if it is too long
+        GlobalProperties.KSTRY_STORY_TRACKING_PARAMS_LENGTH_LIMIT = 50;
+        ParamTracking paramTracking2 = ParamTracking.build(
+                "param", "source",
+                ScopeTypeEnum.REQUEST, "This is a very long string that exceeds the length limit " +
+                        "for serialization",
+                String.class, "converter"
+        );
+
+        FieldSerializeTracking fieldSerializeTracking2 = new FieldSerializeTracking();
+        String serializedValue2 = fieldSerializeTracking2.valueSerialize(paramTracking2);
+        Assert.assertEquals(
+                "This is a very long string that exceeds the length limit for serialization",
+                serializedValue2
+        );
+    }
+}
---
+++ kstry-core/src/test/java/cn/kstry/framework/test/monitor/MonitorTrackingTest.java
@@ -0,0 +1,60 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2023 bytedance.com. All Rights Reserved
+ * This file is part of bytedance EA project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package cn.kstry.framework.test.monitor;
+
+import cn.kstry.framework.core.bpmn.StartEvent;
+import cn.kstry.framework.core.enums.TrackingTypeEnum;
+import cn.kstry.framework.core.monitor.MonitorTracking;
+import cn.kstry.framework.core.monitor.NodeTracking;
+import cn.kstry.framework.core.monitor.SerializeTracking;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+/**
+ * 链路监控测试
+ *
+ * @author xiongzhongwei<xiongzhongwei @ bytedance.com>
+ * @date 12/21/2023 9:40 PM
+ */
+public class MonitorTrackingTest {
+
+
+    /**
+     * Test the node monitoring for success
+     */
+    @Test
+    public void testTrackingLogSuccessfulLog() {
+        // Mock FlowElement
+        StartEvent startEvent = Mockito.mock(StartEvent.class);
+        Mockito.when(startEvent.getId()).thenReturn("testId1");
+        SerializeTracking serializeTracking = Mockito.mock(SerializeTracking.class);
+
+        MonitorTracking monitorTracking = new MonitorTracking(
+                startEvent, TrackingTypeEnum.ALL, serializeTracking
+        );
+        monitorTracking.buildNodeTracking(startEvent);
+        // Act tracking next element
+        monitorTracking.trackingNextElement(startEvent);
+
+        // Act  finishTaskTracking
+        monitorTracking.finishTaskTracking(startEvent, null);
+
+        // Act trackingLog
+        monitorTracking.trackingLog();
+
+        NodeTracking nodeTracking = monitorTracking.getStoryTracking().get(0);
+
+        Assert.assertNotNull(nodeTracking.getEndTime());
+
+        Assert.assertNotSame(nodeTracking.getSpendTime(), monitorTracking.getSpendTime());
+
+        Assert.assertTrue(nodeTracking.getSpendTime() <= monitorTracking.getSpendTime());
+    }
+}
