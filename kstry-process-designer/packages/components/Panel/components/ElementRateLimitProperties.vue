be76b976e9444af9773c8d84ca824107ac562017
lykan <jiashuomeng@gmail.com>
2024-06-28 02:27:06 +0800

    f

---
+++ kstry-process-designer/packages/components/Panel/components/ElementRateLimitProperties.vue
@@ -0,0 +1,22 @@
+<template>
+  <extension-instructions :file-icon="fileIcon" :prefix="prefix" :title-name="titleName" :use-type="useType" />
+</template>
+
+<script>
+import ExtensionInstructions from "@packages/components/Panel/components/SubChild/ExtensionInstructions";
+
+export default {
+  name: "ElementPrevInstructions",
+  data() {
+    return {
+      useType: "prevInstruct",
+      fileIcon: "ArrowRightToLine",
+      titleName: "前置指令",
+      prefix: "^c-"
+    };
+  },
+  components: {
+    ExtensionInstructions
+  }
+};
+</script>
